417783ca62418a08e12f095b1ee49906be8e893f
Ivano Dalmasso <idalmasso@alteanet.it>
2021-07-19 16:59:13 +0200

    first commit with three.js

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -4,13 +4,25 @@
     <div>
       {{ positions }}
     </div>
+    <div id="container"></div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
-
+import * as Three from 'three';
 export default {
+  name: 'Game',
+  static() {
+    return {
+      camera: null,
+      scene: null,
+      renderer: null,
+      mesh: null,
+      now: undefined,
+      createdBox: false
+    };
+  },
   computed: {
     ...mapGetters({
       positions: 'positions'
@@ -20,6 +32,41 @@ export default {
     ...mapActions({
       changeButtonState: 'changeButtonState'
     }),
+    init() {
+      const container = document.getElementById('container');
+
+      this.camera = new Three.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.01, 10);
+      this.camera.position.z = 1;
+      this.scene = new Three.Scene();
+      this.addBox(0.1, 0.1, 0.1, 0, 0.1, 0.1);
+      this.renderer = new Three.WebGLRenderer({ antialias: true });
+      this.renderer.setSize(container.clientWidth, container.clientHeight);
+      container.appendChild(this.renderer.domElement);
+    },
+    animate(timeStamp) {
+      if (this.start === undefined) {
+        this.start = timeStamp;
+      }
+      const elapsed = timeStamp - this.start;
+      requestAnimationFrame(this.animate);
+      this.mesh.rotation.x += 0.01;
+      this.mesh.rotation.y += 0.02;
+      if (elapsed > 3000 && !this.createdBox) {
+        this.addBox(0.2, 0.2, 0.2, -0.1, -0.1, -0.1);
+        this.createdBox = true;
+      }
+      this.renderer.render(this.scene, this.camera);
+    },
+    addBox(x, y, z, posX, posY, posZ) {
+      const geometry = new Three.BoxGeometry(x, y, z);
+      const material = new Three.MeshNormalMaterial();
+
+      this.mesh = new Three.Mesh(geometry, material);
+      this.mesh.position.x = posX;
+      this.mesh.position.y = posY;
+      this.mesh.position.z = posZ;
+      this.scene.add(this.mesh);
+    },
     keyboardHandler(event, pressed) {
       const arrows = code => {
         switch (code) {
@@ -60,6 +107,8 @@ export default {
   mounted() {
     document.addEventListener('keydown', this.keyDown);
     document.addEventListener('keyup', this.keyUp);
+    this.init();
+    this.animate();
   },
   unmounted() {
     document.removeEventListener('keydown', this.keyDown);
@@ -68,4 +117,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+  #container {
+    width: 100%;
+    height: 75vh;
+  }
+</style>
